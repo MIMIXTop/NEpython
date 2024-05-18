c80abc0edb8b764633a0d170693e5d60af016dc5
MIMIXTop <153859950+MIMIXTop@users.noreply.github.com>
2024-05-18 17:55:50 +0300

    Add files via upload

---
+++ fina/niTyxoH/main.py
@@ -0,0 +1,59 @@
+"""
+planets fields
+1.name
+2.type
+3.mass
+4.temperature
+5.radius
+"""
+import os
+from project import ioput, file, dell, search, sort
+mainlist = []
+
+
+def editplanets():
+    pass
+
+
+mmenu = {
+    0: exit,
+    1: ioput.inputplanets,
+    2: dell.removeplanet,
+    3: dell.clearplanets,
+    4: ioput.outputplanets,
+    5: ioput.outputtablet,
+    6: file.saveplanets,
+    7: file.loadpalnets,
+    8: search.searchplanets,
+    9: editplanets,
+    10: sort.sortpalnets
+}
+
+
+def menu():
+    while True:
+        print("0. Exit\n"
+              "1. Add a planet\n"
+              "2. Remove a planet\n"
+              "3. Remove all planets\n"
+              "4. Output planets\n"
+              "5. Output planets like a tablet\n"
+              "6. Save planets\n"
+              "7. Load saved planets\n"
+              "8. Search planets\n"
+              "9. Edit planets\n"
+              "10. Sort planets\n")
+
+        while True:
+            try:
+                nummenu = int(input("Enter your choice: "))
+                if nummenu in mmenu:
+                    mmenu[nummenu](mainlist)
+                break
+            except ValueError:
+                print("Invalid input")
+
+
+os.system('cls')
+file.loadpalnets(mainlist)
+menu()
